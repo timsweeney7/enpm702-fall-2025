09f657c9dca1bf72cd12b4f351f9dccb527e5eb3
Tim Sweeney <timsweeney97@gmail.com>
2025-09-08 20:38:33 -0400

    Added all doxygen files to repo

--- week1/src/week1.cpp
+++ week1/src/week1.cpp
@@ -1,9 +1,9 @@
 /**
  * @file week1.cpp
- * @author Zeid Kootbally (zeidk@umd.edu)
- * @brief Basic hello world example
- * @version 0.1
- * @date 2025-09-01
+ * @author Tim Sweeney (tsweene1@umd.edu)
+ * @brief This is a simmple example for week 1, kyle
+ * @version 1.0
+ * @date 2025-09-02
  * 
  * @copyright Copyright (c) 2025
  * 
@@ -14,8 +14,10 @@
 /**
  * @brief Main function
  * 
- * @return int Return status
+ * @return int 
  */
 int main() {
-  std::cout << "Hello, from enpm702_fall2025!\n";
+  std::cout << "Hello, from epm702_fall2025!\n";
+  std::cout << "what the fuck is up kyle" << '\n';
+  std::cout << "what is up kyle" << '\n';
 }
